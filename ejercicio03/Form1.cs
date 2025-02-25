a5c3245c6a616eb9c77ebe8c6081450ca665a3dd
Jaime <jaimealberto.delagarzarivera@unidemat.edu.mx>
2025-02-25 09:23:46 -0600

    Add files via upload

--- ejercicio03/Form1.cs
+++ ejercicio03/Form1.cs
@@ -8,34 +8,34 @@ namespace ejercicio03
         }
 
         private void btnCalcular_Click(object sender, EventArgs e)
-        {
+        {   //creacion de variables
             double vNum1 = 0.0;
             double vNum2 = 0.0;
             double vResultado = 0.0;
-
+            //conversion de texto a double
             vNum1 = Convert.ToDouble(texnumero1.Text);
             vNum2 = Convert.ToDouble(texnumero2.Text);
-
+            //if para resultado
             if(rbSuma.Checked == true)
-            {
+            {   //suma
                 vResultado = vNum1 + vNum2;
             }
 
             if(rbResta.Checked == true)
-            {
+            {   //resta
                 vResultado = vNum1 - vNum2;
             }
 
             if(rbMulti.Checked == true)
-            {
+            {   //multiplicacion
                 vResultado = vNum1 * vNum2;
             }
             
             if(rbDiv.Checked == true)
-            {
+            {   //divicion
                 vResultado = vNum1 / vNum2;
             }
-
+            //mostrar el resultado segun la eleccion
             lblResultado.Text = vResultado.ToString();
         }
     }
